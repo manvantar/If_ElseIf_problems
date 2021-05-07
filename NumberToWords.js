3ea2c562c2509fcf3b9204604d6ba187bc3a86ee
manvantar <manukvshetty@gmail.com>
2021-05-07 08:44:50 +0530

    Added: program to get number to Words 0 to 9

---
+++ NumberToWords.js
@@ -0,0 +1,45 @@
+let number =Math.floor((Math.random()*10));
+let word="";
+if(number <= 9){
+    if ( number == 0  )
+    {
+            word="zero";
+    }
+    else if ( number == 1 )
+    {
+            word="one";
+    }
+    else if ( number == 2 )
+    {
+            word="two";
+    }
+    else if ( number == 3 )
+    {
+            word="three";
+    }
+    else if ( number == 4 )
+    {
+            word="four";
+    }
+    else if ( number == 5 )
+    {
+            word="five";
+    }
+    else if ( number == 6 )
+    {
+            word="six";
+    }
+    else if ( number == 7 )
+    {
+            word="seven";
+    }
+    else if ( number == 8 )
+    {
+            word="eight";
+    }
+    else 
+    {
+            word="nine";
+    }
+}
+
